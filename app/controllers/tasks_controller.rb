c1c769791717c41ecd81ee27d93f1399fffb243c
momonga.kapibara <silvermomonga@outlook.com>
2019-08-04 09:22:17 +0000

    added show new create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,26 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
-
+  
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(tsk_params)
+
+    if @task.save
+      flash[:success] = 'タスクが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが投稿されませんでした'
+      render :new
+    end    
   end
 
   def edit
@@ -20,3 +32,10 @@ class TasksController < ApplicationController
   def destroy
   end
 end
+
+private
+
+# Strong Parameter
+  def tsk_params
+    params.require(:task).permit(:content)
+  end
