55a0ab88bef45da2f524aab3cce9027ad06a9457
momonga.kapibara <silvermomonga@outlook.com>
2019-08-14 10:43:04 +0000

    removed logged_in from toppage

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,6 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:edit, :update, :show, :destroy]
 
-
   def index
     @tasks = Task.all
   end
