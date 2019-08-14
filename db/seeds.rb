8b0678d0286aaec6eeb7136449b941d04101c3eb
momonga.kapibara <silvermomonga@outlook.com>
2019-08-14 05:34:47 +0000

    added user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..100).each do |number|
+  Task.create(status: 'status ' + number.to_s, content: 'test task ' + number.to_s)
+end
+
+#Task.create(status: 'status 1', content: 'test task 1')
+
